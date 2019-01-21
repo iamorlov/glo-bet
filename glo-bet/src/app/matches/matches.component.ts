6473cd1d794fea02879bd60911d60ab43465a8f6
iamorlov <heiker.ru@gmail.com>
2019-01-21 20:30:44 +0200

    Updated controllers and UI

--- glo-bet/src/app/matches/matches.component.ts
+++ glo-bet/src/app/matches/matches.component.ts
@@ -12,9 +12,17 @@ export class MatchesComponent implements OnInit {
   private getMatchesURL = 'https://global-bet.azurewebsites.net/api/matches?offset=0&limit=100';
   data: any = {};
 
+  private getSingleMatchURL = 'https://global-bet.azurewebsites.net/api/matches/';
+  matchId = false;
+  single_data_array = []; // FUCKING FIX!
+  single_data: any = {};
+
   constructor( private http: Http ) {
     this.getMatchesData();
     this.getMatches();
+
+    this.getSingleMatchData();
+    this.getSingleMatch();
   }
 
   getMatches() {
@@ -26,6 +34,27 @@ export class MatchesComponent implements OnInit {
     this.getMatches().subscribe(data => {
       console.log(data);
       this.data = data;
+    })
+  }
+
+  singleMatchOpen(id) {
+    this.matchId = id;
+    console.log(this.matchId);
+    this.getSingleMatchData();
+    this.getSingleMatch();
+  }
+
+  getSingleMatch() {
+    let getSingleMatchDataURL = this.getSingleMatchURL + this.matchId;
+    return this.http.get(getSingleMatchDataURL)
+    .map((res: Response) => res.json());
+  }
+
+  getSingleMatchData() {
+    this.getSingleMatch().subscribe(data => {
+      console.log(data);
+      this.single_data = data;
+      this.single_data_array.push(data);
     })
   }
 
--- glo-bet/src/app/single-match/single-match.component.ts
+++ glo-bet/src/app/single-match/single-match.component.ts
@@ -5,33 +5,34 @@ import 'rxjs/add/operator/map'
 @Component({
   selector: 'app-single-match',
   templateUrl: './single-match.component.html',
-  styleUrls: ['./single-match.component.css']
+  styleUrls: ['./single-match.component.scss']
 })
 export class SingleMatchComponent implements OnInit {
 
     private getSingleMatchURL = 'https://global-bet.azurewebsites.net/api/matches/';
     matchId = '5c3f25b875001093ac5a25cc';
-    data: any = {};
+    getSingleMatchDataURL = this.getSingleMatchURL + this.matchId;
+    single_data_array = []; // FUCKING FIX!
+    single_data: any = {};
 
     constructor( private http: Http ) {
-      this.getMatchesData();
-      this.getMatches();
+      this.getSingleMatchData();
+      this.getSingleMatch();
     }
 
-    getMatches() {
-      let getSingleMatch = this.getSingleMatchURL + this.matchId;
-      return this.http.get(getSingleMatch)
+    getSingleMatch() {
+      return this.http.get('https://global-bet.azurewebsites.net/api/matches/5c3f25b875001093ac5a25cc')
       .map((res: Response) => res.json());
     }
 
-    getMatchesData() {
-      this.getMatches().subscribe(data => {
+    getSingleMatchData() {
+      this.getSingleMatch().subscribe(data => {
         console.log(data);
-        this.data = data;
+        this.single_data = data;
+        this.single_data_array.push(data);
       })
     }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
